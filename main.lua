77eac3ecec036e317659d8e61c5c4e1e4b64e01a
Kolden12 <143414254+Kolden12@users.noreply.github.com>
2025-12-27 00:44:26 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 local gui          = require "gui"
 local task_manager = require "core.task_manager"
 local settings     = require "core.settings"
+local tracker      = require "core.tracker" -- Added tracker
 
 local local_player, player_position
 
@@ -10,7 +11,6 @@ local function update_locals()
 end
 
 local function main_pulse()
-
     settings:update_settings()
     if not local_player or not settings.enabled then return end
     task_manager.execute_tasks()
@@ -18,13 +18,43 @@ end
 
 local function render_pulse()
     if not local_player or not settings.enabled then return end
+    
     local current_task = task_manager.get_current_task()
+    local px, py, pz = player_position:x(), player_position:y(), player_position:z()
+    
+    -- TIMER OVERLAY: Show wait time remaining on screen
+    local current_os_time = os.time()
+    local wait_time = gui.elements.chest_wait_time:get()
+    
+    if tracker.chest_opened_time ~= nil and current_os_time < tracker.chest_opened_time + wait_time then
+        local remaining = math.ceil((tracker.chest_opened_time + wait_time) - current_os_time)
+        local screen_width = graphics.get_screen_width()
+        local timer_pos = vec2:new(screen_width / 4, 60)
+        graphics.text_2d("POST-CHEST DELAY: " .. remaining .. "s", timer_pos, 20, color_yellow(255))
+    end
+
     if current_task then
-        local px, py, pz = player_position:x(), player_position:y(), player_position:z()
         local draw_pos = vec3:new(px, py - 2, pz + 3)
         graphics.text_3d("Current Task: " .. current_task.name, draw_pos, 14, color_white(255))
     end
 end
+
+BosserPlugin = {
+    enable = function ()
+        console.print('BOSSER ACTIVATING')
+        gui.elements.main_toggle:set(true)
+    end,
+    disable = function ()
+        console.print('BOSSER DEACTIVATING')
+        gui.elements.main_toggle:set(false)
+    end,
+    status = function ()
+        return {
+            ['enabled'] = gui.elements.main_toggle:get(),
+            ['task'] = task_manager.get_current_task()
+        }
+    end,
+}
 
 on_update(function()
     update_locals()
